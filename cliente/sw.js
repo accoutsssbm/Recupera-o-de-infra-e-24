d2925053e9fb6b25d4a222847cd69da18f1b06a5
accoutsssbm <accotsbm@gmail.com>
2026-09-23 09:49:15 -0300

    Refresh engineer app cache for resync update

--- cliente/sw.js
+++ cliente/sw.js
@@ -1,4 +1,4 @@
-const CACHE='acoes-salobo-v8';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./sync.js'];
+const CACHE='acoes-salobo-v9';const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./sync.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();const cs=await self.clients.matchAll({type:'window',includeUncontrolled:true});await Promise.all(cs.filter(c=>c.url.includes('/cliente/')).map(c=>c.navigate(c.url).catch(()=>null)))} )()));
 async function inject(r){let t=await r.text();if(!t.includes('src="sync.js"'))t=t.replace('</body>','<script src="sync.js"></script></body>');const h=new Headers(r.headers);h.set('content-type','text/html; charset=utf-8');h.delete('content-length');return new Response(t,{status:r.status,statusText:r.statusText,headers:h})}
